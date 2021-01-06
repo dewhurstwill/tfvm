88a52dc5f591a32074d9bcec2e7028aa0011b344
Will Dewhurst <34936646+dewhurstwill@users.noreply.github.com>
2021-01-06 09:42:39 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,33 +6,33 @@
 #################################
 
 # Constants
-version='0.1.0'
+version="0.1.0"
 zip_url="https://codeload.github.com/dewhurstwill/tfvm/zip/v${version}"
 zip_download_location="${HOME}/Downloads/tfvm-v${version}.zip"
 bin_download_location="${HOME}/Downloads/tfvm-${version}/dist/tfvm"
 
 # Downloading
-echo 'Downloading'
+echo "Downloading"
 curl -s --output $zip_download_location $zip_url
 
 # Unpacking
-echo 'Unpacking'
+echo "Unpacking"
 unzip -q -d "${HOME}/Downloads" $zip_download_location
 
 # Making binary executable
 chmod +x $bin_download_location
 
 # Installing
-echo 'Installing'
+echo "Installing"
 sudo mv $bin_download_location /usr/local/bin/tfvm
 sudo ln -s ~/.terraform.versions/terraform /usr/local/bin/terraform
 
 # Cleanup
-echo 'Cleaning up'
+echo "Cleaning up"
 rm $zip_download_location
 rm -r "${HOME}/Downloads/tfvm-${version}"
 
-echo 'Do you want to install tfstack-init as well (yes | no)?'
+echo "Do you want to install tfstack-init as well (yes | no)?"
 read tfstack_q
 if [ tfstack_q == 'yes' ]
 then
